55313bb29fb03dff7d22010869763add7bf63eb3
Andrew Shcheglov <godwindock@gmail.com>
2011-08-18 12:41:20 +0300

    Replaced Rails's memoization with ruby's one to get rid of deprecation warning

--- lib/recurly/account.rb
+++ lib/recurly/account.rb
@@ -67,45 +67,42 @@ module Recurly
     end
 
     def charges(status = :all)
-      Charge.list(account_code, status)
+      @charges ||= Charge.list(account_code, status)
     end
-    memoize :charges
 
     def lookup_charge(id)
       Charge.lookup(account_code, id)
     end
 
     def credits
-      Credit.list(account_code)
+      @credits ||= Credit.list(account_code)
     end
-    memoize :credits
 
     def lookup_credit(id)
       Credit.lookup(account_code, id)
     end
 
     def transactions(status)
-      Transaction.list_for_account(account_code, status)
+      @transactions ||= {}
+      @transactions[status] ||= Transaction.list_for_account(account_code, status)
+      @transactions[status]
     end
-    memoize :transactions
 
     def lookup_transaction(id)
       Transaction.lookup(account_code, id)
     end
 
     def invoices
-      Invoice.list(account_code)
+      @invoices ||= Invoice.list(account_code)
     end
-    memoize :invoices
 
     def lookup_invoice(id)
       Invoice.lookup(account_code, id)
     end
 
     def coupon
-      Coupon.find(account_code)
+      @coupon ||= Coupon.find(account_code)
     end
-    memoize :coupon
 
   end
 end
--- lib/recurly/base.rb
+++ lib/recurly/base.rb
@@ -1,5 +1,3 @@
-require 'active_support/memoizable'
-
 module Recurly
 
   class Base < ActiveResource::Base
